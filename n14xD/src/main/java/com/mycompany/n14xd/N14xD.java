10e119403f54f972f313ce31ae14972e3e50db27
jescobarca <79493851+jescobarca@users.noreply.github.com>
2023-03-04 21:58:39 -0500

    Add files via upload

---
+++ n14xD/src/main/java/com/mycompany/n14xd/N14xD.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.n14xd;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jorge
+ */
+public class N14xD {
+
+    public static void main(String[] args) {
+        double a;
+        Scanner ent=new Scanner(System.in);
+        a=ent.nextDouble();
+        System.out.println("Cuadrado:"+(a*a));
+        System.out.println("Cubo:"+(a*a*a));
+    }
+}
